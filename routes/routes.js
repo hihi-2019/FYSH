cd79265ed49b7f55f1b9663551e56c0837e7c954
Joseph <44043180+Josephrw@users.noreply.github.com>
2019-10-31 18:55:57 +1300

    some routes

--- routes/routes.js
+++ routes/routes.js
@@ -9,7 +9,17 @@ const database = require('knex')(config)
 
 
 router.get('/', (req, res) => {
-  res.send('Hi')  
+  res.send('homepage')  
 })
+
+router.get('/listings', (req, res) => {
+  res.send('listings page')  
+})
+
+router.get('/listings/:id', (req, res) => {
+  id = req.params.id
+  res.send('id of listings')  
+})
+
 
 module.exports = router
