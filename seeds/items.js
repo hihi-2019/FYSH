e47c635c7c985b59a895890c54a5807598853a31
Joseph <44043180+Josephrw@users.noreply.github.com>
2019-11-01 15:02:13 +1300

    update seed ids

--- seeds/items.js
+++ seeds/items.js
@@ -1,16 +1,16 @@
 
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex('items').del()
     .then(function () {
       // Inserts seed entries
       return knex('items').insert([
-        {id: 1, name: 'hammer', description: 'Multipurpose, every household must have it', category: 'tools', price: 15.00, user_id: 1, picture: 'hammer.jpg'},
-        {id: 2, name: 'screwdriver', description: 'Another multipurpose miracle that will turn *literally* your world around', category: 'tools', price: 10.00, user_id: 1, picture: 'screwdriver.jpg'},
-        {id: 3, name: 'drill', description: 'Turns things into other things', category: 'tools', price: 25.00, user_id: 1, picture: 'drill.jpg'},
-        {id: 4, name: 'hat', description: 'Something to cover that bald spot of yours', category: 'clothing', price: 19.00, user_id: 2, picture: 'hat.jpg'},
-        {id: 5, name: 'coat', description: 'Make sure you look cool always, everywhere', category: 'clothing', price: 80.00, user_id: 2, picture: 'coat.jpg'},
-        {id: 6, name: 'sofa', description: 'Just to prove that this isnt a cheap scam site. Here something expensive!', category: 'furniture', price: 400.00, user_id: 3, picture: 'sofa.jpg'},
+        { id: 8881, name: 'hammer', description: 'Multipurpose, every household must have it', category: 'tools', price: 15.00, user_id: 1, picture: 'hammer.jpg' },
+        { id: 8882, name: 'screwdriver', description: 'Another multipurpose miracle that will turn *literally* your world around', category: 'tools', price: 10.00, user_id: 1, picture: 'screwdriver.jpg' },
+        { id: 8883, name: 'drill', description: 'Turns things into other things', category: 'tools', price: 25.00, user_id: 1, picture: 'drill.jpg' },
+        { id: 8884, name: 'hat', description: 'Something to cover that bald spot of yours', category: 'clothing', price: 19.00, user_id: 2, picture: 'hat.jpg' },
+        { id: 8885, name: 'coat', description: 'Make sure you look cool always, everywhere', category: 'clothing', price: 80.00, user_id: 2, picture: 'coat.jpg' },
+        { id: 8886, name: 'sofa', description: 'Just to prove that this isnt a cheap scam site. Here something expensive!', category: 'furniture', price: 400.00, user_id: 3, picture: 'sofa.jpg' },
 
       ]);
     });
--- seeds/users.js
+++ seeds/users.js
@@ -1,13 +1,13 @@
 
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex('users').del()
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, name: 'buyer', email: 'buyer@gmail.com', username: 'buyer1234' },
-        {id: 2, name: 'john', email: 'john@gmail.com', username: 'john1234' },
-        {id: 3, name: 'susan', email: 'susan@gmail.com', username: 'susan1234' },
+        { id: 9991, name: 'buyer', email: 'buyer@gmail.com', username: 'buyer1234' },
+        { id: 9992, name: 'john', email: 'john@gmail.com', username: 'john1234' },
+        { id: 9993, name: 'susan', email: 'susan@gmail.com', username: 'susan1234' },
       ]);
     });
 };
