5e03b7a8322597786a3cfdb866a08411144983c4
Joseph <44043180+Josephrw@users.noreply.github.com>
2019-11-01 15:40:59 +1300

    a

--- db/db.js
+++ db/db.js
@@ -32,10 +32,10 @@ function showCategories(db = database){
 function findItem(id, db = database){
   return db('users')
     .leftOuterJoin('items', 'users.id', 'items.user_id')
-    .select('users.name AS user_name', '*')
-    .where('id', id)
+    .select('items.id AS item_id', 'users.name AS user_name', '*')
+    .where('item.id', id)
     .then(item => {
-      //delete item[0].id
+      delete item[0].id
       return item
     })
 }
