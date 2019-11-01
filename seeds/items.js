fa4ddc42bf239efc85434e24b1690e596b43eb81
Joseph <44043180+Josephrw@users.noreply.github.com>
2019-11-01 15:02:38 +1300

    update seed ids

--- seeds/items.js
+++ seeds/items.js
@@ -5,12 +5,12 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('items').insert([
-        { id: 8881, name: 'hammer', description: 'Multipurpose, every household must have it', category: 'tools', price: 15.00, user_id: 1, picture: 'hammer.jpg' },
-        { id: 8882, name: 'screwdriver', description: 'Another multipurpose miracle that will turn *literally* your world around', category: 'tools', price: 10.00, user_id: 1, picture: 'screwdriver.jpg' },
-        { id: 8883, name: 'drill', description: 'Turns things into other things', category: 'tools', price: 25.00, user_id: 1, picture: 'drill.jpg' },
-        { id: 8884, name: 'hat', description: 'Something to cover that bald spot of yours', category: 'clothing', price: 19.00, user_id: 2, picture: 'hat.jpg' },
-        { id: 8885, name: 'coat', description: 'Make sure you look cool always, everywhere', category: 'clothing', price: 80.00, user_id: 2, picture: 'coat.jpg' },
-        { id: 8886, name: 'sofa', description: 'Just to prove that this isnt a cheap scam site. Here something expensive!', category: 'furniture', price: 400.00, user_id: 3, picture: 'sofa.jpg' },
+        { id: 8881, name: 'hammer', description: 'Multipurpose, every household must have it', category: 'tools', price: 15.00, user_id: 9991, picture: 'hammer.jpg' },
+        { id: 8882, name: 'screwdriver', description: 'Another multipurpose miracle that will turn *literally* your world around', category: 'tools', price: 10.00, user_id: 9991, picture: 'screwdriver.jpg' },
+        { id: 8883, name: 'drill', description: 'Turns things into other things', category: 'tools', price: 25.00, user_id: 9991, picture: 'drill.jpg' },
+        { id: 8884, name: 'hat', description: 'Something to cover that bald spot of yours', category: 'clothing', price: 19.00, user_id: 9992, picture: 'hat.jpg' },
+        { id: 8885, name: 'coat', description: 'Make sure you look cool always, everywhere', category: 'clothing', price: 80.00, user_id: 9992, picture: 'coat.jpg' },
+        { id: 8886, name: 'sofa', description: 'Just to prove that this isnt a cheap scam site. Here something expensive!', category: 'furniture', price: 400.00, user_id: 9993, picture: 'sofa.jpg' },
 
       ]);
     });
